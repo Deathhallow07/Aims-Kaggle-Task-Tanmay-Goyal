a3c4d1d1cbc312f37d4debd53635482b094bd091
Deathhallow07 <79511881+Deathhallow07@users.noreply.github.com>
2025-11-12 22:22:04 +0530

    Implement median and mode imputer functions
    Added median and mode imputer functions to handle missing values.

--- third.py
+++ third.py
@@ -32,9 +32,67 @@ def mean_imputer(data):
     return data
 
 
+def median_imputer(data):
+    # ORDINAL ENCODER
+    data_non_numeric = data.select_dtypes('number')
+    cols_to_encode = data_non_numeric.columns
+    median_dict = {}
+    data_list = []
+    for i in cols_to_encode:
+       
+        for j in range(len(data[i])):
+            if pandas.isna(data.loc[j, i]) == True:
+                pass
+            else:
+                data_list.append(data.loc[j, i])
+                
+
+        median_dict[i] = statistics.median(data_list)
+
+
+    for i in cols_to_encode:
+        for j in range(len(data[i])):
+            if pandas.isna(data.loc[j, i]) == True:
+                data.loc[j, i] = median_dict[i]
+            else:
+                pass
+
+        
+          
+   
+    return data
+
+
+def mode_imputer(data):
+    # ORDINAL ENCODER
+    data_non_numeric = data.select_dtypes('number')
+    cols_to_encode = data_non_numeric.columns
+    mode_dict = {}
+    data_list = []
+    for i in cols_to_encode:
+       
+        for j in range(len(data[i])):
+            if pandas.isna(data.loc[j, i]) == True:
+                pass
+            else:
+                data_list.append(data.loc[j, i])
+                
+
+        mode_dict[i] = statistics.mode(data_list)
+
+
+    for i in cols_to_encode:
+        for j in range(len(data[i])):
+            if pandas.isna(data.loc[j, i]) == True:
+                data.loc[j, i] = mode_dict[i]
+            else:
+                pass
+
+
 new_data = mean_imputer(data=data)
 new_data.to_csv("output.csv")
 print(new_data)
+
 
 
 
